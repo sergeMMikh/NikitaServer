6339e15aa36dd81a75da0d05a3ff4d957910e607
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-11-07 23:34:26 +0000

    added: main app

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,7 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls'))
-]
+] + static(settings.STATIC_URL, documents_root=settings.STATIC_ROOT)
