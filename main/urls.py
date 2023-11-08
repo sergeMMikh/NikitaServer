1256348964e9770f2ba4af41f1c9cc27077d63d7
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-11-08 20:38:27 +0000

    added: main app

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about)
+    path('', views.index, name='inicio'),
+    path('about', views.about, name='sobre min')
 
 ]
